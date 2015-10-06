f1928344cb49d06a5013d133296f624641a4a945
github username <github email address>
2015-10-05 21:09:47 -0400

    added changes to the index page

--- thirteenloveinc/config/routes.rb
+++ thirteenloveinc/config/routes.rb
@@ -5,7 +5,29 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  #this is my index
   root 'welcome#homepage'
+
+
+  get 'http://localhost:3000/css/bootstrap.min.css' 
+
+  get 'http://localhost:3000/css/freelancer.css'
+
+  get 'http://localhost:3000/font-awesome/css/font-awesome.min.css'
+
+  get 'http://localhost:3000/assets/javascripts/jquery.js'
+
+  get 'http://localhost:3000/assets/javascripts/bootstrap.min.js'
+
+  get 'http://localhost:3000/assets/javascripts/classie.js'
+
+  get 'http://localhost:3000/assets/javascripts/cbpAnimatedHeader.js'
+
+  get 'http://localhost:3000/assets/javascripts/jqBootstrapValidation.js'
+
+  get 'http://localhost:3000/assets/javascripts/contact_me.js'
+
+  get 'http://localhost:3000/assets/javascripts/freelancer.js'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
